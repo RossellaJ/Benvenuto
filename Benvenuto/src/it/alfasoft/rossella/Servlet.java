d039d7ffd99713f6055da1a1826c2192ff33b49d
Rossella <corso@10.0.1.196>
2016-10-18 09:47:47 +0200

    2

--- Benvenuto/src/it/alfasoft/rossella/Servlet.java
+++ Benvenuto/src/it/alfasoft/rossella/Servlet.java
@@ -1,5 +1,67 @@
 package it.alfasoft.rossella;
 
-public class Servlet {
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class PrimoServlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String nome = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		HttpSession session = request.getSession();
+		PrintWriter writer = response.getWriter();//metodo che f tornare quello che scrivo
+		
+		if(!nome.isEmpty() && nome!=null){
+			
+		session.setAttribute("usernameSalvato", nome);
+		
+			if(password.equals("123")){
+			writer.println("Registrazione avvenuta correttamente! Benvenuto/a "+ nome);
+			}else{
+			response.sendRedirect("login2.html");
+		}
+		
+	}
+	}
 }
+		
+		
+
+
+
+
+
